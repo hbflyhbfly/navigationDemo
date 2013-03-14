7d34b3ee20623a8044bb6573bb2701aea0350875
周飞 <shuufei@shuufeideMacBook-Pro.local>
2013-03-14 22:37:27 +0800

    Initial Commit

---
+++ navigationDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  navigationDemo
+//
+//  Created by 周飞 on 13-3-14.
+//  Copyright (c) 2013年 周飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ navigationDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  navigationDemo
+//
+//  Created by 周飞 on 13-3-14.
+//  Copyright (c) 2013年 周飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
